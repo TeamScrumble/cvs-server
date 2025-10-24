70d3f00829339d1ca3fcf5a51428ff013c80d63d
Jwhyee <kawnsdud@naver.com>
2025-10-24 10:50:57 +0900

    init

---
+++ src/main/kotlin/cvs_server/CvsServerApplication.kt
@@ -0,0 +1,11 @@
+package cvs_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CvsServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CvsServerApplication>(*args)
+}
---
+++ src/test/kotlin/cvs_server/CvsServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package cvs_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CvsServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
