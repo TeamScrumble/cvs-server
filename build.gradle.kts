f675943c78459c02812053c397d3b75ceca2b2cc
wwan13 <wwan13@naver.com>
2025-10-30 22:49:14 +0900

    chore : 전체 모듈에 spring boot 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ subprojects {
     }
 
     dependencies {
+        implementation("org.springframework.boot:spring-boot-starter")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
