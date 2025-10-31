e85da1e896d1eda330fc65e841635bef045ec567
Jwhyee <kawnsdud@naver.com>
2025-10-31 15:56:09 +0900

    feat : CvsTarget에 맞는 Crawler가 동작하도록 서비스 추가

---
+++ crawler/src/main/kotlin/crawler/client/kafka/CrawlerService.kt
@@ -0,0 +1,24 @@
+package crawler.client.kafka
+
+import crawler.client.target.CU
+import crawler.client.target.Emart24
+import crawler.client.target.GS25
+import crawler.client.target.SevenEleven
+import cvs.crawler.CvsTarget
+import org.springframework.stereotype.Service
+
+@Service
+class CrawlerService(
+    private val cu: CU,
+    private val gs25: GS25,
+    private val emart24: Emart24,
+    private val sevenEleven: SevenEleven,
+) {
+    val crawlers = mapOf(
+        CvsTarget.CU to cu,
+        CvsTarget.GS25 to gs25,
+        CvsTarget.EMART_24 to emart24,
+        CvsTarget.SEVEN_ELEVEN to sevenEleven
+    )
+    fun crawl(target: CvsTarget) = crawlers[target]?.run(false) ?: emptyList()
+}
