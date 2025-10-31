6ed6a9fe6ef249b314f854352537fa45d9633f76
wwan13 <wwan13@naver.com>
2025-10-31 11:18:46 +0900

    chore : setting.gradle.kts 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,14 @@ rootProject.name = "cvs-server"
 
 include("core")
 include("core:core-api")
+
 include("clients")
+include("clients:client-common")
 include("clients:client-crawler")
+
 include("storage")
 include("storage:db-core")
-include("clients:client-common")
+include("storage:storage-kafka")
+include("storage:storage-kafka:kafka-event")
+
+include("crawler")
