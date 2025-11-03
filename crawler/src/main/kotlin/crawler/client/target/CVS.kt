6a304faa576bc63d9244bfcdcdc590af2f89fc2c
Jwhyee <kawnsdud@naver.com>
2025-11-03 11:01:23 +0900

    add : 로그 및 시간 체크 기능 추가

--- crawler/src/main/kotlin/crawler/client/target/CVS.kt
+++ crawler/src/main/kotlin/crawler/client/target/CVS.kt
@@ -1,6 +1,8 @@
 package crawler.client.target
 
+import crawler.client.util.calculateTimeMillis
 import cvs.crawler.CrawlerData
+import cvs.crawler.CvsTarget
 import org.openqa.selenium.By
 import org.openqa.selenium.JavascriptExecutor
 import org.openqa.selenium.WebDriver
@@ -8,13 +10,17 @@ import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
+import org.slf4j.LoggerFactory
 import java.time.Duration
+import kotlin.time.measureTimedValue
 
 abstract class CVS {
     companion object {
         const val WAIT_TIMEOUT_SEC = 5L
         const val SLEEP_SHORT_MS = 800L
     }
+
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     fun waitForElement(driver: WebDriver, selector: String, timeoutSec: Long = WAIT_TIMEOUT_SEC) {
         WebDriverWait(driver, Duration.ofSeconds(timeoutSec))
@@ -26,15 +32,27 @@ abstract class CVS {
         Thread.sleep(SLEEP_SHORT_MS)
     }
 
-    fun run(headless: Boolean = false): List<CrawlerData> {
+    fun run(target: CvsTarget, headless: Boolean = false): List<CrawlerData> {
+        val prefix = "Crawling - $target"
+
         val options = ChromeOptions().apply {
             if (headless) addArguments("--headless=new")
         }
         val driver = ChromeDriver(options)
+
         return try {
-            crawl(driver)
+            logger.info("[$prefix] 크롤링 시작")
+            val (value, duration) = measureTimedValue {
+                crawl(driver)
+            }
+
+            logger.info("[$prefix] 크롤링 종료 / 수집된 데이터 : ${value.size}개")
+
+            calculateTimeMillis(logger, prefix, duration.inWholeMilliseconds)
+
+            value
         } catch (e: Exception) {
-            println("크롤링 중 오류 발생: ${e.message}")
+            logger.warn("[$prefix] 크롤링 종료 / 에러 발생 : ${e.message}")
             emptyList()
         } finally {
             driver.quit()
