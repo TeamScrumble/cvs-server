970bf26ff933e5d96930e9606a7c214ab7303b86
wwan13 <wwan13@naver.com>
2025-11-23 16:58:37 +0900

    feat : memberAPi에 확장함수 적용

--- contract/contract-api/client/src/main/kotlin/member/MemberApiClient.kt
+++ contract/contract-api/client/src/main/kotlin/member/MemberApiClient.kt
@@ -2,8 +2,9 @@ package member
 
 import ApiResponse
 import common.Scheme
+import extension.applyHost
+import extension.exchangeToApiResponse
 import kotlinx.coroutines.reactor.awaitSingle
-import org.springframework.core.ParameterizedTypeReference
 import org.springframework.web.reactive.function.client.WebClient
 
 class MemberApiClient(
@@ -18,13 +19,12 @@ class MemberApiClient(
             .uri { builder ->
                 builder
                     .scheme(Scheme.HTTP)
-                    .host(host)
+                    .applyHost(host)
                     .path(MemberAddApi.PATH)
                     .build()
             }
             .bodyValue(request)
-            .retrieve()
-            .bodyToMono(object : ParameterizedTypeReference<ApiResponse<MemberAddApi.Response>>() {})
+            .exchangeToApiResponse<MemberAddApi.Response>()
             .awaitSingle()
     }
 
@@ -35,14 +35,12 @@ class MemberApiClient(
             .uri { builder ->
                 builder
                     .scheme(Scheme.HTTP)
-                    .host("localhost")
-                    .port(8080)
+                    .applyHost(host)
                     .path(MemberGetApi.PATH)
                     .pathSegment(memberId.toString())
                     .build()
             }
-            .retrieve()
-            .bodyToMono(object : ParameterizedTypeReference<ApiResponse<MemberGetApi.Response>>() {})
+            .exchangeToApiResponse<MemberGetApi.Response>()
             .awaitSingle()
     }
 }
