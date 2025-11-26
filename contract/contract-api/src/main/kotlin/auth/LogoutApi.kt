664e2018b319f913a94a98b712ee9033e375561d
wwan13 <wwan13@naver.com>
2025-11-26 17:10:02 +0900

    docs : 기존 API 문서화

--- contract/contract-api/src/main/kotlin/auth/LogoutApi.kt
+++ contract/contract-api/src/main/kotlin/auth/LogoutApi.kt
@@ -1,16 +1,23 @@
 package auth
 
 import ApiResponse
+import docs.Documented
+import io.swagger.v3.oas.annotations.media.Schema
 import passport.Passport
 
 interface LogoutApi {
     companion object {
         const val PATH = "/api/auth/logout"
     }
-
+    @Documented(
+        summary = "로그아웃 API",
+        description = "Access Token을 기반으로 로그아웃 API (프론트에서도 인증 토큰을 지워줘야 함)",
+        response = TokenReissueApi.Response::class,
+    )
     suspend fun logout(passport: Passport): ApiResponse<Response>
 
     data class Response(
+        @Schema(description = "성공 여부", example = "true")
         val success: Boolean,
     )
 }
