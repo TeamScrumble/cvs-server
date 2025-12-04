e2f18c2da7703056d2cf71ee816b50800fb55f95
wwan13 <wwan13@naver.com>
2025-12-04 17:15:35 +0900

    refactor : tokenProvider.decodeToken() 이 BusinessException을 던짇도록 수정

--- contract/contract-error/src/main/kotlin/error/errorcode/AuthErrorCode.kt
+++ contract/contract-error/src/main/kotlin/error/errorcode/AuthErrorCode.kt
@@ -4,8 +4,8 @@ enum class AuthErrorCode(
     override val code: String,
     override val description: String
 ) : ErrorCode {
-    A_001("A_001", "잘못된 인증 토큰 입니다."),
-    A_002("A_002", "잘못된 갱신 토큰 입니다."),
+    A_001("A_001", "잘못된 토큰 입니다."),
+    A_002("A_002", "만료된 토큰 입니다."),
 
     A_003("A_003", "이메일 인증 메일 전송에 실패하였습니다."),
     A_004("A_004", "올바른 이메일 형식이 아님"),
