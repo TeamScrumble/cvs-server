349fdff729e6743839d5856a64718be35130bff4
Jwhyee <kawnsdud@naver.com>
2025-12-05 18:54:06 +0900

    feat : 상품 수동 등록 기능 추가

--- service/product-service/src/main/kotlin/product/product/application/ProductService.kt
+++ service/product-service/src/main/kotlin/product/product/application/ProductService.kt
@@ -3,6 +3,7 @@ package product.product.application
 import cvs.crawler.CrawlerResultEvent
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
 import org.springframework.stereotype.Service
 import product.product.domain.Product
 import product.product.domain.ProductRepository
@@ -11,11 +12,16 @@ import product.product.domain.ProductRepository
 class ProductService(
     private val productRepository: ProductRepository,
 ) {
-    suspend fun save(result: CrawlerResultEvent) {
+    suspend fun saveAll(results: List<CrawlerResultEvent>) = results.sumOf {
+        save(it)
+    }
+
+    suspend fun save(result: CrawlerResultEvent): Int {
         val (target, productList) = result.target to result.data
 
         val entityList = productList.map { p ->
             Product(
+                0L,
                 p.id.toLong(),
                 target,
                 p.productName,
@@ -26,6 +32,6 @@ class ProductService(
             )
         }
 
-        productRepository.saveAll(entityList).collect()
+        return productRepository.saveAll(entityList).toList().size
     }
 }
