2650e5079666e0c931525743ab818119f9700eba
Jwhyee <kawnsdud@naver.com>
2025-12-08 11:37:31 +0900

    feat : 상품 상세 조회 기능 추가

---
+++ contract/contract-api/src/main/kotlin/product/ProductGetApi.kt
@@ -0,0 +1,43 @@
+package product
+
+import ApiResponse
+import docs.Documented
+import io.swagger.v3.oas.annotations.media.Schema
+import passport.Passport
+
+interface ProductGetApi {
+    companion object {
+        const val PATH = "/api/product"
+    }
+
+    @Documented(
+        summary = "상품 조회 API",
+        description = "상품의 상세 정보를 조회하는 API",
+//        request = Request::class,
+        response = Response::class,
+    )
+    suspend fun get(passport: Passport, id: Long): ApiResponse<Response>
+
+    data class Response(
+        @Schema(description = "상품의 id", example = "10")
+        val productId: Long,
+
+        @Schema(description = "편의점 종류", example = "GS25")
+        val cvsTarget: String,
+
+        @Schema(description = "상품명", example = "불닭마요 삼각김밥")
+        val title: String,
+
+        @Schema(description = "이미지 URL", example = "https://gs25.img/samgak.jpg")
+        val img: String,
+
+        @Schema(description = "가격", example = "1500")
+        val price: Int,
+
+        @Schema(description = "행사 정보", example = "1+1")
+        val event: String,
+
+        @Schema(description = "신상품 여부", example = "true")
+        val isNew: Boolean
+    )
+}
--- contract/contract-error/src/main/kotlin/error/errorcode/ProductErrorCode.kt
+++ contract/contract-error/src/main/kotlin/error/errorcode/ProductErrorCode.kt
@@ -1,7 +1,8 @@
 package error.errorcode
 
-class ProductErrorCode(
+enum class ProductErrorCode(
     override val code: String,
     override val description: String
 ) : ErrorCode {
+    P_001("P_001", "등록된 상품이 없습니다."),
 }
