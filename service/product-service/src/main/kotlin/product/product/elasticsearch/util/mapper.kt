65ab6ca84826925571a371c2bcd6391983edec6d
Jwhyee <kawnsdud@naver.com>
2025-12-29 12:27:15 +0900

    chore : 매핑 함수 추가

--- service/product-service/src/main/kotlin/product/product/elasticsearch/util/mapper.kt
+++ service/product-service/src/main/kotlin/product/product/elasticsearch/util/mapper.kt
@@ -1,16 +1,29 @@
 package product.product.elasticsearch.util
 
+import product.product.ProductDto
 import product.product.domain.table.Product
 import product.product.elasticsearch.document.ProductDocument
 
-fun productToDocument(p: Product) = ProductDocument(
-    productId = p.id,
-    cvsProductId = p.cvsProductId,
-    cvsTarget = p.cvsTarget.name,
-    title = p.title,
-    img = p.img,
-    price = p.price,
-    event = p.event,
-    isNewProduct = p.isNewProduct,
-    likeCount = p.likeCount,
+fun Product.toDocument() = ProductDocument(
+    productId = id,
+    cvsProductId = cvsProductId,
+    cvsTarget = cvsTarget.name,
+    title = title,
+    img = img,
+    price = price,
+    event = event,
+    isNewProduct = isNewProduct,
+    likeCount = likeCount,
+)
+
+fun ProductDocument.toDto() = ProductDto(
+    productId = productId,
+    cvsProductId = cvsProductId,
+    cvsTarget = cvsTarget,
+    title = title,
+    img = img,
+    price = price,
+    event = event,
+    isNew = isNewProduct,
+    likeCount = likeCount,
 )
