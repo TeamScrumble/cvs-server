0628ef792cde7c43997300fe613aa6ed8e41238a
Jwhyee <kawnsdud@naver.com>
2026-01-21 16:03:21 +0900

    refactor : 기존 Scheduler 활성화 어노테이션을 Application 파일로 이동 및 캐싱 어노테이션 추가

--- service/product-service/src/main/kotlin/product/ProductServiceApplication.kt
+++ service/product-service/src/main/kotlin/product/ProductServiceApplication.kt
@@ -2,7 +2,11 @@ package product
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableCaching
+@EnableScheduling
 @SpringBootApplication(
     scanBasePackages = ["product", "security", "db", "docs", "messagebroker"]
 )
